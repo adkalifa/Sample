2b35e2dca7f1ee1506e262209f47dd1b9f06fbfa
adkalifa <adkalifa@gmail.com>
2019-12-16 15:46:09 +0530

    added

---
+++ .js
@@ -0,0 +1,13 @@
+	filemode = true
+	bare = false
+	logallrefupdates = true
+[remote "origin"]
+	url = https://github.com/adkalifa/Sample/
+	fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+	remote = origin
+	merge = refs/heads/master
+[user]
+	name = adkalifa
+	email = adkalifa@gmail.com
+webcrafters@webcrafters:~/code/Sample$ code.
